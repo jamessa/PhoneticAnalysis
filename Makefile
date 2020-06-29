8409bcd5eb55d01ff91287a97b1642ed8f058308
james sa <james@keto.tw>
2020-06-29 20:01:18 +0800

    make it speak

---
+++ Makefile
@@ -0,0 +1,2 @@
+speak:
+	python to_ipa.py |  python lexconvert.py --phones2phones unicode-ipa espeak | espeak
--- to_ipa.py
+++ to_ipa.py
@@ -3,12 +3,16 @@ from pypinyin import pinyin, Style
 # fixme: 目前的用regex取代的方式有很大的問題，因為符號取代錯誤，
 # 因此還是用lex and yacc比較好
 print(pinyin('此', style=Style.IPA))
-assert('tsʰɨ' == pinyin('此', style=Style.IPA)[0][0])
+# assert('tsʰɨ' == pinyin('此', style=Style.IPA)[0][0])
 
 
 with open('hsbdmj.txt', 'r') as f:
     for line in f:
         p = pinyin(line, errors='ignore', style=Style.IPA)
         # print(line)
-        # print(p)
+
+        for w in p:
+            print(w[0], end=' ')
+
+
 
